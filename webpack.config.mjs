861121ead68f251024a0cc7c9fc64f899e51ac49
blwatkins <blwatkins@users.noreply.github.com>
2026-03-26 14:04:30 -0500

    Update webpack.config.mjs

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -47,7 +47,7 @@ export default {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'sketches',
+            title: 'Three.js Scene',
             inject: 'body',
             favicon: './assets/icon/favicon.ico'
         }),
